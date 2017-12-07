3f11337a85bcfd72330c4d0cc0584fa15b685677
Martin Kotevski <kotevski.martin.1996@gmail.com>
2017-12-07 02:41:12 +0100

    1st commit

---
+++ lab6_problem2_queries.sql
@@ -0,0 +1,77 @@
+/*
+    1. Najdi gi sifrite i iminjata na site vraboteni i imeto na pretstavata
+       vo koja igrale glavna uloga za koja se prodadeni bileti zza odrzuvanje
+       na folema scena kako redovna pretstava.
+*/
+
+SELECT V.V, IMEV, IMEP
+FROM (((VRABOTENI V JOIN ULOGI U ON V.V=U.V)
+       JOIN PRETSTAVI P ON U.P=P.P)
+       JOIN BILETI B ON B.P=P.P)
+WHERE U.IMA_ULOGA='glavna' AND B.STATUS='redovna' AND B.SCENA='golema';
+
+/*
+    2. Najdi go akterot koj ucestvuval vo najgolem broj na pretstavi
+*/
+SELECT V, IMEV
+FROM ULOGI NATURAL JOIN VRABOTENI
+GROUP BY V, IMEV
+HAVING COUNT(*) >= (SELECT MAX(COUNT(*))
+                    FROM ULOGI
+                    GROUP BY V
+                    );
+
+
+/*
+    3. Najdi go imeto na akterot so najvisok prosecen honorar
+*/
+SELECT IMEV
+FROM ZARABOTUVA NATURAL JOIN VRABOTENI
+GROUP BY V, IMEV
+HAVING AVG(HONORAR) >= ALL(SELECT AVG(HONORAR)
+                           FROM ZARABOTUVA
+                           GROUP BY V
+                          );
+
+/*
+    3. Version 2:
+
+SELECT IMEV
+FROM VRABOTENI
+WHERE V IN (SELECT V
+            FROM ZARABOTUVA
+            GROUP BY V
+            HAVING AVG(HONORAR) >= ALL(SELECT AVG(HONORAR)
+                                       FROM ZARABOTUVA
+                                       GROUP BY V
+                                      )
+            );
+*/
+
+
+/*
+    4. Da se vrati pregled na brojot na razlicni pretstavi sto se igrale posebno
+       za sekoj grad, sortirani po imeto na gradot.
+*/
+
+SELECT GRAD, COUNT(DISTINCT P)
+FROM BILETI NATURAL JOIN TEATRI
+GROUP BY GRAD
+ORDER BY GRAD;
+
+
+/*
+    5. Prikazi gi pretstavnicite za koi vkupniot profit od prodazba na bileti
+       e pogolem od vkupnata suma isplatena kako honorar
+*/
+SELECT IMEP
+FROM PRETSTAVI
+WHERE P IN (SELECT P
+            FROM BILETI B
+            GROUP BY P
+            HAVING SUM(CENA*PRODADENI) > (SELECT SUM(HONORAR)
+                                          FROM ZARABOTUVA
+                                          WHERE P = B.P
+                                          GROUP BY P
+                                          )
+);
